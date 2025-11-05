5c7183d385f1d2b2e3048d3713f894a92844ebb3
IvanPogi <ivanmacaraig2@gmail.com>
2025-11-05 13:16:22 +0800

    TAPOS NA LOP

--- flynt.py
+++ flynt.py
@@ -11,36 +11,29 @@ LETTERS_DIGITS = LETTERS + DIGITS
 
 
 class Error:
-    def __init__(self, pos_start, pos_end, error_name, details):
-        self.pos_start = pos_start
-        self.pos_end = pos_end
-        self.error_name = error_name
-        self.details = details
-
-    def as_string(self):
-        result = f"{self.error_name}: {self.details}"
-        result += f"\nFile {self.pos_start.fn}, line {self.pos_start.ln + 1}"
-        result += "\n\n" + string_with_arrows(
-            self.pos_start.ftxt, self.pos_start, self.pos_end
-        )
-        return result
-
+	def __init__(self, pos_start, pos_end, error_name, details):
+		self.pos_start = pos_start
+		self.pos_end = pos_end
+		self.error_name = error_name
+		self.details = details
+	
+	def as_string(self):
+		result  = f'{self.error_name}: {self.details}\n'
+		result += f'File {self.pos_start.fn}'
+		result += '\n\n' + string_with_arrows(self.pos_start.ftxt, self.pos_start, self.pos_end)
+		return result
 
 class IllegalCharError(Error):
-    def __init__(self, pos_start, pos_end, details):
-        super().__init__(
-            pos_start, pos_end, "Bisaya mag code ampota, bawal yan: ", details
-        )
-
+	def __init__(self, pos_start, pos_end, details):
+		super().__init__(pos_start, pos_end, 'Illegal Character', details)
 
 class ExpectedCharError(Error):
-    def __init__(self, pos_start, pos_end, details):
-        super().__init__(pos_start, pos_end, "Expected Character", details)
-
+	def __init__(self, pos_start, pos_end, details):
+		super().__init__(pos_start, pos_end, 'Expected Character', details)
 
 class InvalidSyntaxError(Error):
-    def __init__(self, pos_start, pos_end, details=""):
-        super().__init__(pos_start, pos_end, "Invalid Syntax", details)
+	def __init__(self, pos_start, pos_end, details=''):
+		super().__init__(pos_start, pos_end, 'Invalid Syntax', details)
 
 
 class RuntimeError(Error):
@@ -62,10 +55,7 @@ class RuntimeError(Error):
         ctx = self.context
 
         while ctx:
-            result = (
-                f" File {pos.fn}, line {str(pos.ln + 1)}, in {ctx.display_name}\n"
-                + result
-            )
+            result = f'  File {pos.fn}, in {ctx.display_name}\n' + result
             pos = ctx.parent_entry_pos
             ctx = ctx.parent
 
@@ -76,25 +66,25 @@ class RuntimeError(Error):
 
 
 class Position:
-    def __init__(self, idx, ln, col, fn, ftxt):
-        self.idx = idx
-        self.ln = ln
-        self.col = col
-        self.fn = fn
-        self.ftxt = ftxt
+	def __init__(self, idx, ln, col, fn, ftxt):
+		self.idx = idx
+		self.ln = ln
+		self.col = col
+		self.fn = fn
+		self.ftxt = ftxt
 
-    def advance(self, current_char=None):
-        self.idx += 1
-        self.col += 1
+	def advance(self, current_char=None):
+		self.idx += 1
+		self.col += 1
 
-        if current_char == "\n":
-            self.ln += 1
-            self.col = 0
+		if current_char == '\n':
+			self.ln += 1
+			self.col = 0
 
-        return self
+		return self
 
-    def copy(self):
-        return Position(self.idx, self.ln, self.col, self.fn, self.ftxt)
+	def copy(self):
+		return Position(self.idx, self.ln, self.col, self.fn, self.ftxt)
 
 
 # TOKENS
@@ -125,25 +115,24 @@ KEYWORDS = ["SET", "ALSO", "OR", "NO", "IF", "THEN", "ELIF", "ELSE"]  # AND  # O
 
 
 class Token:
-    def __init__(self, type_, value=None, pos_start=None, pos_end=None):
-        self.type = type_
-        self.value = value
+	def __init__(self, type_, value=None, pos_start=None, pos_end=None):
+		self.type = type_
+		self.value = value
 
-        if pos_start:
-            self.pos_start = pos_start.copy()
-            self.pos_end = pos_start.copy()
-            self.pos_end.advance()
+		if pos_start:
+			self.pos_start = pos_start.copy()
+			self.pos_end = pos_start.copy()
+			self.pos_end.advance()
 
-        if pos_end:
-            self.pos_end = pos_end.copy()
+		if pos_end:
+			self.pos_end = pos_end.copy()
 
-    def matches(self, type_, value):
-        return self.type == type_ and self.value == value
-
-    def __repr__(self):
-        if self.value:
-            return f"{self.type}:{self.value}"
-        return f"{self.type}"
+	def matches(self, type_, value):
+		return self.type == type_ and self.value == value
+	
+	def __repr__(self):
+		if self.value: return f'{self.type}:{self.value}'
+		return f'{self.type}'
 
 
 # LEXER
@@ -159,9 +148,7 @@ class Lexer:
 
     def advance(self):
         self.pos.advance(self.current_char)
-        self.current_char = (
-            self.text[self.pos.idx] if self.pos.idx < len(self.text) else None
-        )
+        self.current_char = self.text[self.pos.idx] if self.pos.idx < len(self.text) else None
 
     def make_tokens(self):
         tokens = []
@@ -212,6 +199,7 @@ class Lexer:
                 char = self.current_char
                 self.advance()
                 return [], IllegalCharError(pos_start, self.pos, "'" + char + "'")
+            
         tokens.append(Token(TT_EOF, pos_start=self.pos))
         return tokens, None
 
